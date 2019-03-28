154d844c9aba7208887fedfa980b17b848fde8a4
Brian Tomlinson <btomlins@redhat.com>
2019-03-27 20:19:13 -0500

    Update the available spec fields for our CR

--- pkg/apis/greeting/v1alpha1/greeting_types.go
+++ pkg/apis/greeting/v1alpha1/greeting_types.go
@@ -12,6 +12,7 @@ type GreetingSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	Message string `json:message`
+	Size    int32  `json:size`
 }
 
 // GreetingStatus defines the observed state of Greeting
